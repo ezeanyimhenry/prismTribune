8625238e9341a0d9f659394239ab54314fe1a899
Ezeanyim henry <ezeanyimhenry@gmail.com>
2025-04-22 22:48:40 +0100

    feat: blog theme setup

---
+++ resources/views/layouts/blog.blade.php
@@ -0,0 +1,312 @@
+<!doctype html>
+<html>
+<!-- Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+
+@include('layouts.inc.head')
+
+<body>
+    <div id="main" class="header-style1">
+
+        @include('layouts.inc.header')
+
+        @yield('content')
+
+        @include('layouts.inc.footer')
+
+    </div><!-- End Main -->
+
+    <!-- Mobile Menu -->
+    <nav id="mobile-nav">
+        <div>
+            <ul>
+                <li>
+                    <a href="index.html">Home</a>
+                    <ul>
+                        <li><a href="index.html">Home - All Block</a></li>
+                        <li>
+                            <a href="#">Home - Grid Style</a>
+                            <ul>
+                                <li><a href="grid_sidebar.html">With Sidebar</a></li>
+                                <li><a href="grid.html">No Sidebar</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Home - Blog Style</a>
+                            <ul>
+                                <li><a href="blog_classic.html">Classic Blog</a></li>
+                                <li><a href="blog_modern.html">Modern Blog</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Home - Minimal Style</a>
+                            <ul>
+                                <li><a href="minimal.html">2 Columns</a></li>
+                                <li><a href="minimal_sidebar.html">2 Columns With Sidebar</a></li>
+                                <li><a href="minimal_full.html">3 Columns</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Home - Fixed Sidebar</a>
+                            <ul>
+                                <li><a href="fixed_left.html">Fixed Left Sidebar</a></li>
+                                <li><a href="fixed_right.html">Fixed Right Sidebar</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Home - 3 Different Columns</a>
+                            <ul>
+                                <li><a href="left_big.html">Left Big Column</a></li>
+                                <li><a href="right_big.html">Right Big Column</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="grid.html">Tech</a>
+                    <ul>
+                        <li><a href="#">Reviews</a></li>
+                        <li><a href="#">Apps</a></li>
+                        <li><a href="#">Design</a></li>
+                        <li><a href="#">Gadgets</a></li>
+                        <li><a href="#">Mobile</a></li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="minimal_blog.html">Lifestyle</a>
+                    <ul>
+                        <li><a href="#">Love</a></li>
+                        <li><a href="#">Advice</a></li>
+                        <li><a href="#">Food</a></li>
+                        <li><a href="#">Travel</a></li>
+                        <li><a href="#">Fashion</a></li>
+                    </ul>
+                </li>
+
+                <li><a href="blog_modern.html">Business</a></li>
+
+                <li><a href="header-style3.html">Entertainment</a></li>
+
+                <li>
+                    <a href="#">Features</a>
+                    <ul>
+                        <li>
+                            <a href="index.html">Header Style</a>
+                            <ul>
+                                <li><a href="index.html">Header Style 1</a></li>
+                                <li><a href="header-style2.html">Header Style 2</a></li>
+                                <li><a href="header-style3.html">Header Style 3</a></li>
+                                <li><a href="fixed_left.html">Header Style 4</a></li>
+                            </ul>
+                        </li><!-- .dropdown-submenu -->
+                        <li>
+                            <a href="index.html">Footer Style</a>
+                            <ul>
+                                <li><a href="index.html#footer">Footer Style 1</a></li>
+                                <li><a href="footer-style2.html#footer">Footer Style 2</a></li>
+                            </ul>
+                        </li><!-- .dropdown-submenu -->
+
+                        <li>
+                            <a href="#">Pages</a>
+                            <ul>
+                                <li><a href="page.html">About Page</a></li>
+                                <li><a href="page_contact.html">Contact Page</a></li>
+                                <li><a href="page_author.html">Author page</a></li>
+                                <li><a href="page_tags.html">Tags page</a></li>
+                                <li><a href="page_search.html">Search page</a></li>
+                                <li><a href="page_404.html">404 page</a></li>
+                            </ul>
+                        </li><!-- .dropdown-submenu -->
+
+                        <li>
+                            <a href="#">Posts</a>
+                            <ul>
+                                <li><a href="post_simple.html">Simple Post</a></li>
+                                <li><a href="post_review.html">Review Post</a></li>
+                                <li><a href="post_parallax.html">Parallax post</a></li>
+                                <li><a href="post_fullwidth.html">Full Width Post</a></li>
+                                <li><a href="post_fixed.html">Fixed Sidebar Post</a></li>
+                                <li><a href="post_slider.html">Gallery Post</a></li>
+                                <li><a href="post_video.html">Video Post</a></li>
+                            </ul>
+                        </li><!-- .dropdown-submenu -->
+
+                        <li>
+                            <a href="#">Category Layouts</a>
+                            <ul>
+                                <li><a href="blog_classic.html">Classic blog</a></li>
+                                <li><a href="blog_modern.html">Modern blog</a></li>
+                                <li><a href="grid_sidebar.html">Grid blog</a></li>
+                                <li><a href="minimal_blog.html">Minimal blog</a></li>
+                                <li><a href="fixed_blog.html">Fixed sidebar blog</a></li>
+                                <li><a href="big_blog.html">Left big column blog</a></li>
+                            </ul>
+                        </li><!-- .dropdown-submenu -->
+
+                        <li><a href="page_typography.html">Typography</a></li>
+                        <li><a href="page_shortcodes.html">Shortcodes</a></li>
+                    </ul><!-- dropdown-menu -->
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- / Mobile Menu -->
+    <div id="go-top-button" class="fa fa-angle-up" title="Scroll To Top"></div>
+
+    <div class="mobile-overlay" id="mobile-overlay"></div>
+
+
+
+    <!-- Style Customizer -->
+
+    <div class="customizer">
+
+        <div id="customizer-button" class="customizer-button">
+
+            <i class="fa fa-cog"></i>
+
+        </div>
+
+        <div class="customizer-content clearfix">
+
+            <div class="customizer-item clearfix">
+
+                <h5>Layout Style</h5>
+
+                <ul>
+
+                    <li class="active" data-class="boxed"><span>Boxed</span></li>
+
+                    <li data-class="wide"><span>Wide</span></li>
+
+                </ul>
+
+            </div>
+
+
+
+            <div class="customizer-item clearfix">
+
+                <h5>Template Style</h5>
+
+                <ul>
+
+                    <li class="active" data-class="light"><span>Light</span></li>
+
+                    <li data-class="dark-skin"><span>Dark</span></li>
+
+                </ul>
+
+            </div>
+
+
+
+            <div class="customizer-item clearfix">
+
+                <h5>Sticky Header</h5>
+
+                <ul>
+
+                    <li class="active" data-class="fixed-header"><span>Enable</span></li>
+
+                    <li data-class="relative-header"><span>Disable</span></li>
+
+                </ul>
+
+            </div>
+
+
+
+            <div class="customizer-item clearfix customizer-colors">
+
+                <h5>Template Color</h5>
+
+                <ul>
+
+                    <li class="template-color1 active" data-class="body-color1"><span></span></li>
+
+                    <li class="template-color2" data-class="body-color2"><span></span></li>
+
+                    <li class="template-color3" data-class="body-color3"><span></span></li>
+
+                    <li class="template-color4" data-class="body-color4"><span></span></li>
+
+                    <li class="template-color5" data-class="body-color5"><span></span></li>
+
+                    <li class="template-color6" data-class="body-color6"><span></span></li>
+
+                </ul>
+
+            </div>
+
+
+
+            <div class="customizer-item clearfix customizer-colors customizer-bg">
+
+                <h5>Background Images</h5>
+
+                <ul>
+
+                    <li class="no-background" data-class="no-background" class="active"><span></span></li>
+
+                    <li class="background-image1" data-class="background-image1"><span></span></li>
+
+                    <li class="background-image2" data-class="background-image2"><span></span></li>
+
+                    <li class="background-image3" data-class="background-image3"><span></span></li>
+
+                    <li class="background-image4" data-class="background-image4"><span></span></li>
+
+                </ul>
+
+            </div>
+
+            <div class="customizer-item clearfix">
+                <a href="http://themeforest.net/item/admag-responsive-blog-magazine-html-template/11147481?ref=digitaltheme"
+                    class="btn btn-default btn-purchase">
+                    Purchase
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+    <!-- Jquery js -->
+
+    <script src="js/jquery-1.11.2.min.js"></script>
+
+
+
+    <!-- Modernizr -->
+
+    <script src="js/modernizr.min.js"></script>
+
+
+
+    <!-- Bootstrap js -->
+
+    <script src="plugins/bootstrap/js/bootstrap.js"></script>
+
+
+
+    <!-- Google map api -->
+
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+
+
+
+    <!-- Theme js -->
+
+    <script src="js/script.min.js"></script>
+
+
+
+</body>
+
+</html>
---
+++ resources/views/layouts/inc/head.blade.php
@@ -0,0 +1,46 @@
+<head>
+    <meta charset="utf-8">
+    <title>ADMAG | Responsive Blog & Magazine HTML Template</title>
+    <meta name="keywords" content="HTML5 Template">
+    <meta name="description" content="ADMAG — Responsive Blog & Magazine HTML Template">
+    <meta name="author" content="digitaltheme.co">
+    <meta class="viewport" name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="img/favicon/android-chrome-192x192.png" sizes="192x192">
+
+    <!-- Google Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Roboto:100,300,300italic,400,400italic,500,700,700italic,900"
+        rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Noto+Serif:400,400italic,700,700italic" rel="stylesheet"
+        type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Raleway:900" rel="stylesheet" type="text/css">
+
+    <!-- Icon Font -->
+    <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="css/style.min.css">
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
